38403fdc2b9b2ccc817d9493fc41f7f626bd5b7f
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-01-10 11:23:00 +0200

    update spinner.js file

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const Spinner = () => {
   return (
     <div className=" flex justify-center items-center">
@@ -6,4 +7,5 @@ const Spinner = () => {
     </div>
   );
 };
+
 export default Spinner;
