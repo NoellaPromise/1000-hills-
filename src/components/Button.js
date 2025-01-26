009c16e3078c8fe0c4fb276f8d89168bdd114eaf
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2025-01-26 09:23:00 +0200

    update homephoto.js file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Button = (props) => {
   let classes =
     "px-8 py-px bg-transparent hover:bg-main-button-color text-main-button-color hover:text-white border border-main-button-color hover:border-opacity-0 font-medium rounded";
@@ -13,5 +12,4 @@ const Button = (props) => {
     </button>
   );
 };
-
 export default Button;
